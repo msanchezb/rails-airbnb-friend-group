c702425f351ef39d63284ec36d896eb561c4920e
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-08-20 17:21:14 +0100

    routes and controllers

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,8 @@
+class RequestsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get "events/index/:location/:date", to: "events#index", as: :events
+
+  resources :events, only: [:new, :create, :edit, :update, :destroy, :show] do
+    resources :requests, only: [:create, :destroy, :update]
+  end
+
+  resources :users, except: [:destroy, :index]
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
