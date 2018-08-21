330d46ec5909af21fdb10db571f183293262910b
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-08-21 12:56:57 +0100

    very basic events controller and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,12 @@
 class EventsController < ApplicationController
 
-  require 'open-uri'
+  # require 'open-uri'
+
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    #for the moment listing them all
+    @events = Event.all
   end
 
   def new
@@ -11,54 +14,52 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    if @event.update(event_params)
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @event.destroy
   end
 
   private
 
   def event_params
-    params.require(:event).permit(:date, :location, :description, :)
-
+    params.require(:event).permit(:date, :location, :description, :category, :group_size, :latitude, :longitude)
   end
 
   def set_event
-    @event =
+    @event = Event.find(params[:id])
   end
 
 
-  def get_location(address)
-    url = "https://maps.googleapis.com/maps/api/geocode/json?#{adress}";
-    doc = open(url).read
-    doc = JSON.parse(doc)
-    latitude = doc[:data][:results][0][:geometry][:location][:lat]
-  end
+  # def get_location(address)
+  #   url = "https://maps.googleapis.com/maps/api/geocode/json?#{adress}";
+  #   doc = open(url).read
+  #   doc = JSON.parse(doc)
+  #   latitude = doc[:data][:results][0][:geometry][:location][:lat]
+  #   longitude = doc[:data][:results][0][:geometry][:location][:lng]
+  #   { latitude: latitude, longitude: longitude }
+  # end
 end
-
-create_table "events", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "date"
-    t.string "location"
-    t.text "description"
-    t.boolean "confirmed", default: false
-    t.text "review_of_host"
-    t.text "review_of_guest"
-    t.string "category"
-    t.integer "group_size"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
-    t.index ["user_id"], name: "index_events_on_user_id"
-  end
 
