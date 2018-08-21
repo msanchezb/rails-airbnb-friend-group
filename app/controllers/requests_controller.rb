6873f4ec0c0de40a14d3fd78bd9180a1ae505918
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-08-21 11:42:58 +0100

    Requests controller updated

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,8 +1,30 @@
 class RequestsController < ApplicationController
+
+  before_action :set_request, only: :destroy
+
   def create
+    @request.new(request_params)
+    @request.user = current_user
+    @request.event = params[:event_id]
+    if @request.save
+      redirect_to
+    else
+      render "events/#{params[:event_id]}"
+    end
   end
 
   def destroy
+    @request.destroy
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:message)
+  end
+
+  def set_request
+    @request = Request.find(params[:id])
   end
 
 end
