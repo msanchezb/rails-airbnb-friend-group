0b9dc0c569699c98ab466085625467b40b1afed1
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-08-21 17:02:11 +0100

    name column added to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :users, through: :requests
 
 
-  validates_presence_of :user_id, :date, :location, :description, :category, :group_size
+  validates_presence_of :user_id, :date, :location, :description, :category, :group_size, :name
   validates :description, length: { minimum: 20 }
   validates :category, inclusion: { in: %w[drinking sports cinema theatre museum outdoors] }
 end
