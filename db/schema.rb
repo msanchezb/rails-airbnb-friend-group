535e8926f8a4a155545d99e836bd0a42c6dd2446
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-08-21 10:55:37 +0100

    Added latitude and longitude floats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_143540) do
+ActiveRecord::Schema.define(version: 2018_08_21_093034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2018_08_20_143540) do
     t.integer "group_size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
